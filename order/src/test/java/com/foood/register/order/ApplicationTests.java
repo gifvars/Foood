d6355c67395a209ff5597ccb08194a3060f3b02f
jan.gifvars <jan.gifvars@cgi.com>
2025-10-24 08:32:11 +0200

    Permission denied for Shakir, testing commit.

--- order/src/test/java/com/foood/register/order/ApplicationTests.java
+++ order/src/test/java/com/foood/register/order/ApplicationTests.java
@@ -1,13 +1,22 @@
 package com.foood.register.order;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.testng.Assert;
+
+//@SpringBootTest
+// @EnableWebMvc
+// @AutoConfiguration
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+    @Test
+    public void testCalculate(){
+
+        Assert.assertTrue(true);
+
+    }
 }
