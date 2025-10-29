60a269e5a0a5a32ed39c24cf4ef1f1db8be8219d
Jan Gifvars <jan@gifvars.com>
2025-10-29 13:14:01 +0100

    Fixat med docker, kafka och kafka-ui.

--- order/src/main/java/com/foood/register/order/controller/RegisterOrderController.java
+++ order/src/main/java/com/foood/register/order/controller/RegisterOrderController.java
@@ -4,12 +4,12 @@ package com.foood.register.order.controller;
 import com.foood.commons.dto.Order;
 import com.foood.register.order.service.RegisterOrderService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import jakarta.validation.constraints.NotNull;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:5173")
+// TODO @CrossOrigin(origins = "http://localhost:5173")
 @RestController
-@RequestMapping("/foood")
 public class RegisterOrderController {
 
     private RegisterOrderService registerOrderService;
@@ -18,8 +18,8 @@ public class RegisterOrderController {
         this.registerOrderService = registerOrderService;
     }
 
-    @PostMapping("/register/order")
-    public ResponseEntity<String> create(@NotNull @RequestBody Order order) {
+    @PostMapping("/foood/register/order")
+    public ResponseEntity<String> create(@RequestBody Order order) {
         System.out.println(order);
         registerOrderService.registerOrder(order);
         return ResponseEntity.ok().body("{\"respons\": \"Success\"}");
