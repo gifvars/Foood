eec7e7e5552a1a6c25c0dca0c8bdca828509fa2a
Jan Gifvars <jan@gifvars.com>
2025-11-10 15:19:05 +0100

    Smärre förändringar.

--- order-svc/src/main/java/com/foood/register/order/repository/OrderRepository.java
+++ order-svc/src/main/java/com/foood/register/order/repository/OrderRepository.java
@@ -4,13 +4,6 @@ package com.foood.register.order.repository;
 import com.foood.register.order.model.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 public interface OrderRepository extends MongoRepository<Order, String> {
-
-
-    //List<Order> findById(String id);
-    //boolean existsByEmail(String email);
-
 
 }
--- order-svc/src/test/java/com/foood/register/order/ApplicationTests.java
+++ order-svc/src/test/java/com/foood/register/order/ApplicationTests.java
@@ -1,13 +1,24 @@
 package com.foood.register.order;
 
+import com.foood.commons_svc.dto.CartItem;
+import com.foood.commons_svc.enums.OrderStatus;
+import com.foood.register.order.service.Dto2EntityTranslator;
 import org.junit.jupiter.api.Test;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.testng.Assert;
 
-//@SpringBootTest
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Collections;
+
+@SpringBootTest
 // @EnableWebMvc
 // @AutoConfiguration
 class ApplicationTests {
+
+    @Autowired
+    Dto2EntityTranslator dto2EntityTranslator;
 
 	@Test
 	void contextLoads() {
@@ -17,6 +28,27 @@ class ApplicationTests {
     public void testCalculate(){
 
         Assert.assertTrue(true);
+
+    }
+
+    @Test
+    public void testTransform(){
+
+        var orderRec = new com.foood.commons_svc.dto.Order(
+                666,
+                Collections.singletonList(new CartItem(222, 1, BigDecimal.valueOf(125))),
+                        BigDecimal.valueOf(230),
+                        BigDecimal.valueOf(999),
+                        BigDecimal.valueOf(10),
+                OrderStatus.ACCEPTED_BY_RESTAURANT,
+                777,
+                888,
+                LocalDateTime.parse("2025-11-06T14:32:00"),
+                        LocalDateTime.parse("2025-11-06T14:32:00"));
+
+        var order = dto2EntityTranslator.translateOrder(orderRec);
+        Assert.assertTrue(order.getOrderItems().size() == 1);
+        Assert.assertTrue(order.getDriverId() == 888);
 
     }
 }
